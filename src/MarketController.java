fd25fa017cbe7f22819eeefb48d86c13499053b0
ChicheT <tsai122@purdue.edu>
2020-02-24 00:40:17 -0500

    update MarketController

--- src/MarketController.java
+++ src/MarketController.java
@@ -1,10 +1,42 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.text.Text;
 
 public class MarketController {
+    @FXML
+    private Text good1Text;
+    @FXML
+    private Text good2Text;
+    @FXML
+    private Text good3Text;
+    @FXML
+    private Text good1PriceText;
+    @FXML
+    private Text good2PriceText;
+    @FXML
+    private Text good3PriceText;
+    @FXML
+    private Spinner Good1Spinner;
+    @FXML
+    private Spinner Good2Spinner;
+    @FXML
+    private Spinner Good3Spinner;
 
+    private Good good[];
 
+    public void initialize(){
+        GoodGenerater goodGenerater = new GoodGenerater();
+        good = goodGenerater.getGood();
+        good1Text.setText(good[0].getName());
+        good2Text.setText(good[1].getName());
+        good3Text.setText(good[2].getName());
+        good1PriceText.setText("$"+good[0].getBase_price());
+        good2PriceText.setText("$"+good[1].getBase_price());
+        good3PriceText.setText("$"+good[2].getBase_price());
 
+    }
 
     public void characterUpgradeBtnPressed(ActionEvent actionEvent) {
     }
